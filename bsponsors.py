63c191b5b5dd624011368e2b04a1da3cd758aef2
toni_heittola <toni.heittola@gmail.com>
2019-03-19 21:11:10 +0200

    Make yaml loading compatible with v5.1

--- bsponsors.py
+++ bsponsors.py
@@ -110,8 +110,13 @@ def load_sponsors_registry(source):
 
     if source and os.path.isfile(source):
         try:
-            with open(source, 'r') as field:
-                sponsors_registry = yaml.load(field)
+            from distutils.version import LooseVersion
+            if LooseVersion(str(yaml.__version__)) >= "5.1":
+                with open(source, 'r') as field:
+                    sponsors_registry = yaml.load(field, Loader=yaml.FullLoader)
+            else:
+                with open(source, 'r') as field:
+                    sponsors_registry = yaml.load(field)
 
             if 'data' in sponsors_registry:
                 sponsors_registry = sponsors_registry['data']
